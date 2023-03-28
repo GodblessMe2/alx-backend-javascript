b07f74d984cdccb5ec02ea1ce275071d4131f875
GodblessMe2 <oshindeinde@gmail.com>
2023-03-28 16:32:32 +0100

    correction

--- 0x03-ES6_data_manipulation/100-weak.js
+++ 0x03-ES6_data_manipulation/100-weak.js
@@ -1,8 +1,8 @@
 export const weakMap = new WeakMap();
 
 export default function queryAPI(endpoint) {
-  let called = 0;
-  if (weakMap.get(endpoint)) called = weakMap.get(endpoint);
+  const maxCalls = 5;
+  const called = weakMap.get(endpoint) || 0;
   weakMap.set(endpoint, called + 1);
-  if (called + 1 >= 5) throw new Error('Endpoint load is high');
+  if (called + 1 >= maxCalls) throw new Error('Endpoint load is high');
 }
